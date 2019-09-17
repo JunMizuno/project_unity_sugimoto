d941b274f1bc6ef1be64ff0d07503b1b95ccdd33
水野淳 <>
2019-09-17 11:46:04 +0900

    ジャイロの数値取得テスト(apkビルド後の実機確認)

---
+++ unity_sugimoto/Assets/Scripts/Accelerate/AccelerateManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerateManager : MonoBehaviour
+{
+    private float accelerateValueX = default;
+    public float AccelerateValueX
+    {
+        get
+        {
+            return accelerateValueX;
+        }
+    }
+    private float accelerateValueY = default;
+    public float AccelerateValueY
+    {
+        get
+        {
+            return accelerateValueY;
+        }
+    }
+    private float accelerateValueZ = default;
+    public float AccelerateValueZ
+    {
+        get
+        {
+            return accelerateValueZ;
+        }
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    private void Update()
+    {
+        accelerateValueX = Input.acceleration.x;
+        accelerateValueY = Input.acceleration.y;
+        accelerateValueZ = Input.acceleration.z;
+    }
+}
---
+++ unity_sugimoto/Assets/Scripts/StepsCounter/StepsCounterLabel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StepsCounterLabel : MonoBehaviour
+{
+    [SerializeField]
+    public AccelerateManager accelerateManager = default;
+
+    /// <summary>
+    /// 初期設定
+    /// </summary>
+    private void Start()
+    {
+        InitializeStepsCounterLabel();
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    private void Update()
+    {
+        if (accelerateManager != null)
+        {
+            RefreshStepsCounterLabel();
+        }
+    }
+
+    /// <summary>
+    /// ラベル表示の初期化
+    /// </summary>
+    private void InitializeStepsCounterLabel()
+    {
+        var label = this.GetComponent<Text>();
+        label.text = string.Format("Acceleration x:{0:F1}  y:{1:F1}  z:{2:F1}", 0, 0, 0);
+    }
+
+    /// <summary>
+    /// ラベル表示の更新
+    /// </summary>
+    private void RefreshStepsCounterLabel()
+    {
+        float x = accelerateManager.AccelerateValueX;
+        float y = accelerateManager.AccelerateValueY;
+        float z = accelerateManager.AccelerateValueZ;
+
+        var label = this.GetComponent<Text>();
+        label.text = string.Format("Acceleration x:{0:F1}  y:{1:F1}  z:{2:F1}", x, y, z);
+    }
+}
